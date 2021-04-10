1b6796af06c53c9376583a3c48ac93552282908a
Daniel Crafts <dc@bbtronic.de>
2021-04-11 00:10:11 +0200

    added reboot check

--- hyperv_reboot.ps1
+++ hyperv_reboot.ps1
@@ -1,8 +1,17 @@
+	$value = test-path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+	 
+	if ($value -match "False") {
+	echo "OK - No Reboot required"
+	$returnCode=0
+	}
+	
+	else {
 	Stop-VM * -Force
 	$count = ((get-vm).Name | measure).count
-	$offline = ((get-vm).State -like "Off" | measure).count
 	do{
 		start-sleep -s 60
+		$offline = ((get-vm).State -like "Off" | measure).count
 	  }
 	while($offline -ne $count)
 	Restart-Computer
+	}
