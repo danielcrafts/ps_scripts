7ace62ce55a0ff23b02eea1be618a0bf26087562
Daniel Crafts <dc@bbtronic.de>
2021-04-10 23:49:37 +0200

    normal reboot script

---
+++ hyperv_reboot.ps1
@@ -0,0 +1,8 @@
+	Stop-VM * -Force
+	$count = ((get-vm).Name | measure).count
+	$offline = ((get-vm).State -like "Off" | measure).count
+	do{
+		start-sleep -s 60
+	  }
+	while($offline -ne $count)
+	#Restart-Computer
---
+++ reboot.ps1
@@ -0,0 +1,15 @@
+	$value = test-path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
+	 
+	# If path does not exist, return OK status
+	if ($value -match "False") {
+	echo "OK - No Reboot required"
+	$returnCode=0
+	}
+	 
+	# Else return WARNING status
+	else {
+	Restart-Computer
+	$returnCode=1
+	}
+	 
+	exit ($returnCode)
