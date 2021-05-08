5e0ac7da8131458a56c69f29604500c43285ae1d
Daniel Crafts <dc@bbtronic.de>
2021-05-09 01:59:28 +0200

    update to reboot all in same script

--- reboot.ps1
+++ reboot.ps1
@@ -1,15 +1,28 @@
+	param
+	(
+	$typ = "0"
+	)
 	$value = test-path -path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired"
 	 
-	# If path does not exist, return OK status
-	if ($value -match "False") {
-	echo "OK - No Reboot required"
-	$returnCode=0
-	}
-	 
-	# Else return WARNING status
-	else {
+	#$typ=Client
+	if (($value -match "true") -and ((Get-CimInstance Win32_OperatingSystem -Property *).Name -match "Business") -and ($typ -match "Client")) {
 	Restart-Computer -Force
-	$returnCode=1
 	}
-	 
-	exit ($returnCode)
+	
+	#$typ=Hyperv
+	if (($value -match "true") -and ((Get-CimInstance Win32_OperatingSystem -Property *).Name -match "Server") -and ($typ -match "HyperV") -and ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V).state -match "enabled")) {
+	Stop-VM * -Force
+	$count = ((get-vm).Name | measure).count
+	do{
+		start-sleep -s 60
+		$offline = ((get-vm).State -like "Off" | measure).count
+	  }
+	while($offline -ne $count)
+	Restart-Computer -Force
+	}
+
+	#$typ=Server
+	if (($value -match "true") -and ((Get-CimInstance Win32_OperatingSystem -Property *).Name -match "Server") -and ($typ -match "Server") -and ((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V).state -match "Disabled")) {
+	Restart-Computer -Force
+	}
+	
